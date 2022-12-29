34b477484649e71c5bd506776fabcfe332a28d24
Samuel <samuel.sb04g@gmail.com>
2022-12-29 16:12:22 -0300

    testing ads

--- src/components/TheValues.jsx
+++ src/components/TheValues.jsx
@@ -62,9 +62,24 @@ export function TheValues(){
         </div>
       </div>
 
-      <div className={styles.anuncios}>
-        anuncio
-      </div>
+      {/* <div className={styles.anuncios}>
+        <script async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6078704893663548"
+          crossorigin="anonymous"
+          >
+        </script>
+        <ins class="adsbygoogle"
+            style="display:block;backiground-color:rgb(200,200,200);"
+            data-ad-client="ca-pub-6078704893663548"
+            data-ad-slot="7142047328"
+            data-adtest="on"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+          </ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </div> */}
 
     {/* </div> */}
     </>
